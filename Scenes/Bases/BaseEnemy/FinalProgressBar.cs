ef9632c088c0d726b741d003806d50d6947282fb
Никита Зуев <nikitazuev238@gmail.com>
2026-04-03 21:43:03 +0400

    Work enemy healthBar

--- Scenes/Bases/BaseEnemy/FinalProgressBar.cs
+++ Scenes/Bases/BaseEnemy/FinalProgressBar.cs
@@ -2,24 +2,31 @@ using Godot;
 
 public partial class FinalProgressBar : BaseProgressBarScript
 {
-    private BaseEnemyScript _enemy;
+	private BaseEnemyScript _enemy;
 
-    public override void _Ready() 
-    {
-        base._Ready();
-        ShowPercentage = false;
-        
-        _enemy = GetNode<BaseEnemyScript>("../../");
-        if (_enemy == null)
-        {
-            return;
-        }
+	public override void _Ready() 
+	{
+		base._Ready();
+		ShowPercentage = false;
+		
+		_enemy = GetNode<BaseEnemyScript>("../../");
+		if (_enemy == null)
+		{
+			return;
+		}
 
-        if (_enemy != null)
-        {
-            MaxValue = _enemy.GetMaxHp();
-            Value = _enemy.GetCurrentHp();
-        }
-    }
+		if (_enemy != null)
+		{
+			MaxValue = _enemy.GetMaxHp();
+			Value = _enemy.GetCurrentHp();
+		}
+	}
+	
+	public override void _Process(double delta)
+	{
+		if (_enemy != null)
+		{
+			Value = _enemy.GetCurrentHp();
+		}
+	}
 }
-
--- Scenes/Entityes/Enemies/Mannequin/MannequinScript.cs
+++ Scenes/Entityes/Enemies/Mannequin/MannequinScript.cs
@@ -3,34 +3,34 @@ using System;
 
 public partial class MannequinScript : BaseEnemyScript
 {
-    protected override int MaxHp => 50;
+	protected override int MaxHp => 50;
 
-    private EnteredWall _wall;
+	private EnteredWall _wall;
 
-    private bool isDied = false;
-    public override void _Ready()
-    {
-        _wall = GetNode<EnteredWall>("../EnteredWall");
-        base._Ready();
-    }
+	private bool isDied = false;
+	public override void _Ready()
+	{
+		_wall = GetNode<EnteredWall>("../EnteredWall");
+		base._Ready();
+	}
 
-    public override void Die()
-    {
-        if(!isDied) _wall.QueueFree();
-        isDied = true;
-    }
+	public override void Die()
+	{
+		if(!isDied) _wall.QueueFree();
+		isDied = true;
+	}
 
-    public override void TakeDamage(int value)
-    {
-        _sprite.Play("hurt");
-        base.TakeDamage(value);
-        var timer = GetTree().CreateTimer(1f);
-        timer.Timeout += () => {
-            if (IsInstanceValid(this)) {
-                _sprite.Play("stay");
-            }
-        };
+	public override void TakeDamage(int value)
+	{
+		_sprite.Play("hurt");
+		base.TakeDamage(value);
+		var timer = GetTree().CreateTimer(1f);
+		timer.Timeout += () => {
+			if (IsInstanceValid(this)) {
+				_sprite.Play("stay");
+			}
+		};
 
-    }
+	}
 
 }
